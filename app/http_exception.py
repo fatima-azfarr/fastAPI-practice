6380239c2d10d510f6bd2cc05d3cf245e0d4d815
Fatima Azfar <fatimaazfar310@gmail.com>
2026-08-06 08:05:49 +0500

    Add http_exception.py - practice with FastAPI's HTTPException for error handling

---
+++ app/http_exception.py
@@ -0,0 +1,56 @@
+from typing import Any
+
+from fastapi import FastAPI, HTTPException,status
+from scalar_fastapi import get_scalar_api_reference
+
+app = FastAPI()
+
+shipments = {
+     12345 : {
+         "content" : "wooden box",
+         "weight" : 20,
+         "status" : "in transit"
+    },
+
+     12346 : {
+          "content" : "glassware",
+          "weight" : 10,
+          "status" : "placed"
+    },
+     12347 : {
+          "content" : "mobile phones",
+          "weight" : 50,
+          "status" : "placed"
+    },
+     12348 : {
+          "content" : "keyboard",
+          "weight" : 10,
+          "status" : "cancelled"
+        }
+
+}
+
+@app.get("/shipment")
+def get_shipment(id : int | None = None) -> dict[str,Any]:
+
+    if id is None:
+        id = max(shipments.keys())
+        return shipments[id]
+
+    # for when the id doesnt exit - we raise an httpexception
+    if id not in shipments:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "Given id doesn't exits!"
+
+        )
+    
+    else:
+        return shipments[id]
+
+@app.get("/scalar")
+def get_scalar_doc():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title = "scalar"
+    )

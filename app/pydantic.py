5e47ecbaca7551bc46b0c58992db5ae296830e23
Fatima Azfar <fatimaazfar310@gmail.com>
2026-08-08 02:43:43 +0500

    pydantic.py - refactor CRUD with Pydantic BaseModel

---
+++ app/pydantic.py
@@ -0,0 +1,103 @@
+from typing import Any
+
+from fastapi import FastAPI, HTTPException, status
+from scalar_fastapi import get_scalar_api_reference
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Shipment(BaseModel):
+    content: str
+    weight: float
+    status: str
+    destination: str
+
+
+shipments = {
+    12345: {"content": "wooden box", "weight": 20, "status": "in transit"},
+    12346: {"content": "glassware", "weight": 10, "status": "placed"},
+    12347: {"content": "mobile phones", "weight": 50, "status": "placed"},
+    12348: {"content": "keyboard", "weight": 10, "status": "cancelled"},
+}
+
+
+# GET METHOD
+@app.get("/shipment")
+def get_shipment(id: int | None = None) -> dict[str, Any]:
+    if id is None:
+        id = max(shipments.keys())
+        return shipments[id]
+
+    if id not in shipments:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Invalid request, given id doesn't exist!",
+        )
+    return shipments[id]
+
+
+# POST METHOD
+@app.post("/shipment")
+def submit_shipments(body: Shipment) -> dict[str, Any]:
+    if body.weight > 25:
+        raise HTTPException(
+            status_code=status.HTTP_406_NOT_ACCEPTABLE,
+            detail="Invalid request, weight isn't under the designated limit (i.e 25 kgs)",
+        )
+
+    new_id = max(shipments.keys()) + 1
+    shipments[new_id] = body.model_dump()
+    return {"id": new_id}
+
+
+# PUT METHOD
+@app.put("/shipment/{id}")
+def update_shipment(id: int, body: Shipment) -> dict[str, Any]:
+    if id not in shipments:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Invalid request, the given id doesn't exist!",
+        )
+
+    shipments[id] = body.model_dump()
+    return shipments[id]
+
+
+# PATCH METHOD
+@app.patch("/shipment/{id}")
+def patch_shipment(id: int, body: Shipment) -> dict[str, Any]:
+    if id not in shipments:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Invalid request, the given id doesn't exist!",
+        )
+
+    shipment = shipments[id]
+    if body.content is not None:
+        shipment["content"] = body.content
+    if body.weight is not None:
+        shipment["weight"] = body.weight
+    if body.status is not None:
+        shipment["status"] = body.status
+
+    shipments[id] = shipment
+    return shipments[id]
+
+
+# DELETE METHOD
+@app.delete("/shipment/{id}")
+def delete_shipment(id: int) -> dict[str, Any]:
+    if id not in shipments:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Invalid request, the given id doesn't exist!",
+        )
+    deleted_shipment = shipments.pop(id)
+    return {"detail": f"The shipment #{id} is deleted", "shipment": deleted_shipment}
+
+
+# SCALAR API Documentation
+@app.get("/scalar", include_in_schema=False)
+def scalar_documentation():
+    return get_scalar_api_reference(openapi_url=app.openapi_url, title="scalar API")

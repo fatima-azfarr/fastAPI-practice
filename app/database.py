088a8b241cfb659bc8744c58551ad2ca03ccc244
Fatima Azfar <fatimaazfar310@gmail.com>
2026-08-11 07:57:27 +0500

    feat: add JSON database persistence

---
+++ app/database.py
@@ -0,0 +1,18 @@
+import json
+from pathlib import Path
+
+shipments = {}
+
+FILE_PATH = Path(__file__).parent / "shipments.json"
+
+if FILE_PATH.exists() and FILE_PATH.stat().st_size > 0:
+    with open(FILE_PATH, "r") as f:
+        data = json.load(f)
+
+        for value in data:
+            shipments[value["id"]] = value
+
+
+def save():
+    with open(FILE_PATH, "w") as f:
+        json.dump(list(shipments.values()), f, indent=4)
